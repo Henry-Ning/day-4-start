85c74edf3f02a6a9b20c448149b2628db4bbcb41
Henry Ning <henryhuning@gmail.com>
2020-12-29 09:39:33 +0000

    42. [Interactive Coding Exercise] Banker Roulette - Who will pay the bill? 10min

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import random
 
-coin_toss = random.randint(0,1)
-if coin_toss ==1:
-  print("head")
-else:
-  print("tail")
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
 
 
-states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
-
-print(states_of_america)
-
-
-dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
-
+random_number = random.randint(0, len(names)-1)
+print(names[random_number])
