9b9a2961fb4ae8b7bde79e020979b38317108d31
Henry Ning <henryhuning@gmail.com>
2020-12-31 08:37:33 +0000

    Quiz 5: List and IndexError Quiz

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ vegetables = ["kale", "celery"]
 dirty_dozon = [fruits, vegetables]
 
 print(dirty_dozon)
+
+# What you knowThese are questions you got right on the first try.
+# Given the following list:fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]Which line of code will give you "Apples"?
+# Given the code below:fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"] fruits[-1] = "Melons" fruits.append("Lemons") print(fruits)What do you think will be printed?
+# What you should review
+# Given the code below:fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"] vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"] dirty_dozen = [fruits, vegetables] print(dirty_dozen[1][1])What will be printed?
+# Lecture 43 IndexErrors and Working with Nested Lists
